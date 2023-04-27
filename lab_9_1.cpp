800539a5515c5b0da3ece2927bcc03cc024b2714
Sehaj-kahlon <sehajkahlon437@gmail.com>
2023-04-28 03:15:48 +0530

    0_1 knapsack using memoization

--- lab_9_1.cpp
+++ lab_9_1.cpp
@@ -1,36 +1,41 @@
-// 1 Write a program in Python to solve 0 1 knapsack using Dynamic programming.
+// 1 Write a program to solve 0 1 knapsack using Dynamic programming.
 #include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 const int n1 = 1e9 + 7;
 #define ll long long
-
+// 0_1 knapsack using memoization
+int knapsack_m(int n, int w, vector<pair<int, int>> v)
+{
+    if (w <= 0)
+    {
+        return 0;
+    }
+    if (n <= 0)
+    {
+        return 0;
+    }
+    // cananot take the item
+    if (v[n - 1].first > w)
+    {
+        return knapsack_m(n - 1, w, v);
+    }
+    return max(knapsack_m(n - 1, w, v), knapsack_m(n - 1, w - v[n - 1].first, v) + v[n - 1].second);
+}
 int main()
 {
     // inputs
     // number of items
     int n;
     cin >> n;
+    // weight of the bag
+    int w;
+    cin >> w;
     // weights and profit
-    vector<pair<int, int>> v;
+    vector<pair<int, int>> v(n);
     for (int i = 0; i < n; i++)
     {
         cin >> v[i].first >> v[i].second;
     }
-    // weight of the bag
-    int w;
-    cin >> w;
-    // create a dp table (2-D vector) of n_1 rows and w+1 columns
-    vector<vector<int>> dp[n + 1][w + 1];
-    // make the elements of 0th row and 0th column as zero
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < w; j++)
-        {
-            dp[0][j] = 0;
-        }
-        dp[i][0] = 0;
-    }
-
     return 0;
 }
