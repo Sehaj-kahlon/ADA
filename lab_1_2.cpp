b0a9dd0931ec77dda220903acf873874534030b2
Sehaj-kahlon <sehajkahlon437@gmail.com>
2023-04-28 10:21:57 +0530

    explained lab 1 ques 2

--- lab_1_2.cpp
+++ lab_1_2.cpp
@@ -1,4 +1,4 @@
-// Patlu and Motu works in a building construction, they have to put some number of bricks  in a building construction, they have to put some number of bricks N from one place to another, and started doing their work. They decided , they end up with a fun challenge who will put the last brick. one place to another, and started doing their work. 
+// Patlu and Motu works in a building construction, they have to put some number of bricks  in a building construction, they have to put some number of bricks N from one place to another, and started doing their work. They decided , they end up with a fun challenge who will put the last brick. one place to another, and started doing their work.
 // They to follow a simple rule, In the i'th round, Patlu puts i bricks whereas Motu puts ix2 bricks. There are only N bricks, you need to help find the challenge result to find who put the last brick you need to help find the challenge result to find who put the last brick.
 // Input: First line contains an integer N.
 #include <iostream>
@@ -14,16 +14,20 @@ int main()
     int x = n;
     for (int i = 1; i <= (n / 3); i++)
     {
+        // at each iteration update the value of number of bricks leaft
         x = x - (3 * i);
         // cout<<x;
+        // if number of bricks left after a certain iteration is zero then motu wins since he was the last one to place the brciks
         if (x == 0)
         {
             cout << "MOTU" << endl;
             break;
         }
+        // if after a certain iteration the number of bricks is -ve find the value of previos left bricks
         else if (x < 0)
         {
             x = x + 3 * i;
+            // if(x>i) motu sins since patlu will place only i bricks and there will be bricks left for motu
             if (x > i)
             {
                 cout << "MOTU" << endl;
