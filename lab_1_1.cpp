995f2c13e727c9976d48fe2f579e36b7331aaafe
Sehaj-kahlon <sehajkahlon437@gmail.com>
2023-04-28 10:16:52 +0530

    time and space complexit analysis

--- lab_1_1.cpp
+++ lab_1_1.cpp
@@ -17,6 +17,7 @@ const int n1 = 1e9 + 7;
 #define ll long long
 const int mod = 1000000007;
 class solve
+// using recursion
 {
 public:
     int findStep(int n)
@@ -29,6 +30,25 @@ public:
         else
             return findStep(n - 3) + findStep(n - 2) + findStep(n - 1);
     }
+    // memoization
+    int findStep_dp(int n, vector<int> &dp)
+    {
+        if (n == 0)
+        {
+            return 1;
+        }
+        else if (n <= 0)
+        {
+            return 0;
+        }
+        // if the value is already presnet there
+        if (dp[n] != -1)
+        {
+            return dp[n];
+        }
+        // else calculate and save value in dp
+        return dp[n] = findStep_dp(n - 3, dp) + findStep_dp(n - 2, dp) + findStep_dp(n - 1, dp);
+    }
 };
 
 int main()
@@ -37,5 +57,9 @@ int main()
     cin >> n;
     solve obj;
     cout << obj.findStep(n) << endl;
+    vector<int> dp(n + 1, -1);
+    cout << obj.findStep_dp(n, dp) << endl;
     return 0;
 }
+// tc : o(3^n); sc: o(1) - recursive
+//tc : dp - memoization o(n) -- sc -- o(n)
